36a568cd1d18038a1e6c67ef5c1ac5205cc40a8d
airportyh <airportyh@gmail.com>
2011-08-25 17:07:05 -0400

    Ported to IE from 7+ I believe. Also fixed bug with initial double refresh on local files.

--- js/super_refresh.js
+++ js/super_refresh.js
@@ -1,19 +1,16 @@
-var mainURL, urlBar, frame, scriptTags, cssLinkTags
-var frameLoadTime, intervalID
-var lastFetchedTimes = {}
-var contentHashes = {}
-var compareMode = 'Content'
-var scrollOffset
-var isChrome = navigator.userAgent.match(/Chrome/) != null
-if (!window.console)
-    window.console = {log: function(){}}
-
-function toArray(thing){
-    var ret = []
-    for (var i = 0; i < thing.length; i++)
-        ret.push(thing[i])
-    return ret
-}
+var mainURL, 
+    urlBar, 
+    frame, 
+    scriptTags, 
+    cssLinkTags,
+    frameLoadTime, 
+    intervalID,
+    lastFetchedTimes = {},
+    contentHashes = {},
+    isLocal = window.location.protocol === 'file:',
+    compareMode = isLocal ? 'Content' : 'LastModified',
+    scrollOffset,
+    cantAccessIframe = false
 
 function addTS(url, date){
     return url + '?' + (date.getTime())
@@ -32,12 +29,21 @@ function ajax(url, method, callback){
     xhr.send()
 }
 
+function filter(array, func){
+    var ret = []
+    for (var i = 0, len = array.length; i < len; i++){
+        var item = array[i]
+        if (func(item))
+            ret.push(item)
+    }
+    return ret
+}
+
 function stripQS(url){
     var i = url.indexOf('?')
     if (i < 0) return url
     return url.substring(0, i)
 }
-
 function ifModified(url, onModified){
     url = stripQS(url)
     var randUrl = addRandom(url)
@@ -47,16 +53,16 @@ function ifModified(url, onModified){
                 if (compareMode == 'LastModified'){
                     var lastModified = new Date(this.getResponseHeader('Last-Modified'))
                     var lastFetched = lastFetchedTimes[url] || frameLoadTime
-                    //console.log('lastModified: ' + String(lastModified) + ', ' + lastModified)
                     if (lastModified.getTime() > lastFetched.getTime()){
                         onModified(addTS(url, lastModified))
                         lastFetchedTimes[url] = lastModified
                     }
                 }else{
-                    var sha1 = sha1Hash(this.responseText)
-                    if (contentHashes[url] != sha1){
-                        onModified(randUrl)
-                        contentHashes[url] = sha1
+                    var content = this.responseText
+                    if (contentHashes[url] != content){
+                        if (url in contentHashes)
+                            onModified(randUrl)
+                        contentHashes[url] = content
                     }
                 }
             }
@@ -65,83 +71,106 @@ function ifModified(url, onModified){
 }
 
 function refresh(){
-    if (isChrome){
+    if (cantAccessIframe){
         frame.src = mainURL
-    }else{
-        ifModified(mainURL, function(url){
-            frame.src = url
+        return
+    }
+    ifModified(mainURL, function(url){
+        frame.src = url
+    })
+    for (var i = 0, len = cssLinkTags.length; i < len; i++){
+        var link = cssLinkTags[i]
+        var url = link.href
+        ifModified(url, function(url){
+            link.href = url
         })
-        cssLinkTags.forEach(function(link){
-            var url = link.href
-            ifModified(url, function(url){
-                link.href = url
-            })
-        })
-    
-        scriptTags.forEach(function(script){
-            var url = script.src
-            ifModified(url, function(url){
-                frame.contentDocument.location.reload()
-            })
+    }
+
+    for (var i = 0, len = scriptTags.length; i < len; i++){
+        var script = scriptTags[i]
+        var url = script.src
+        ifModified(url, function(url){
+            if (cantAccessIframe)
+                frame.src = mainURL
+            else
+                frame.contentWindow.document.location.reload()
         })
     }
 }
 
 function setFrameHeight(){
-    frame.style.height = (window.innerHeight - frame.offsetTop) + 'px'
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+    frame.style.height = (height - frame.offsetTop) + 'px'
 }
 function onWindowResize(){
     setFrameHeight()
 }
 function setTitle(){
-    document.title = 'SuperRefresh - ' + frame.contentDocument.title
+    try{
+        document.title = 'SuperRefresh - ' + frame.contentDocument.title
+    }catch(e){
+        document.title = 'SuperRefresh - ' + document.title
+    }
+    
 }
 
 function onFrameLoaded(e){
-    mainURL = frame.contentWindow.location.toString()
+    try{
+        mainURL = frame.contentWindow.location.toString()
 
-    urlBar.innerHTML = stripQS(mainURL)
-    frameLoadTime = new Date()
-    setTitle()
-    var doc = frame.contentDocument
-    var frameWindow = frame.contentWindow
-    var frameDocument = frame.contentDocument
+        urlBar.innerHTML = stripQS(mainURL)
+    
+        frameLoadTime = new Date()
+        setTitle()
+        var doc = frame.contentDocument
+        var frameWindow = frame.contentWindow
+        var frameDocument = frameWindow.document
 
-    scriptTags = toArray(frameDocument.getElementsByTagName('script'))
-        .filter(function(tag){
-            return Boolean(tag.src)
-        });
+        scriptTags = filter(frameDocument.getElementsByTagName('script'),
+            function(tag){
+                return Boolean(tag.src)
+            })
 
-    cssLinkTags = toArray(frameDocument.getElementsByTagName('link'))
-        .filter(function(tag){
-            return tag.rel == 'stylesheet'
+        cssLinkTags = filter(frameDocument.getElementsByTagName('link'),
+            function(tag){
+                return tag.rel == 'stylesheet'
+            })
+
+
+        if (scrollOffset)
+            frameWindow.scrollTo(scrollOffset.x, scrollOffset.y)
+
+        onEvent(frameWindow, 'scroll', function(){
+            scrollOffset = {x: frameWindow.pageXOffset, y: frameWindow.pageYOffset}
         })
-
-
-    if (scrollOffset)
-        frameWindow.scrollTo(scrollOffset.x, scrollOffset.y)
-
-    frameWindow.addEventListener('scroll', function(){
-        scrollOffset = {x: frameWindow.pageXOffset, y: frameWindow.pageYOffset}
-    }, false)
-    //console.log(cssLinkTags.map(function(tag){return tag.href}).join("\n"))
+    }catch(e){
+        cantAccessIframe = true
+        mainURL = window.location.toString()
+        urlBar.innerHTML = stripQS(mainURL)
+    }
 
     if (!intervalID)
-        intervalID = setInterval(refresh, 1000)
+        intervalID = setInterval(refresh, cantAccessIframe ? 3000 : 1000)
 }
 
 function removeChildren(elm){
-    toArray(elm.childNodes).forEach(function(child){
-        elm.removeChild(child)
-    })
+    for (var i = 0, len = elm.childNodes.length; i < len; i++){
+        var child = elm.childNodes[i]
+        if (child) elm.removeChild(child)
+    }
 }
-
-
 
 function blankOutPage(){
     var head = document.getElementsByTagName('head')[0]
     removeChildren(head)
     document.body.innerHTML = ''
+}
+
+function onEvent(elm, event, callback){
+    if (elm.addEventListener)
+        elm.addEventListener(event, callback, false)
+    else if (elm.attachEvent)
+        elm.attachEvent('on' + event, callback)
 }
 
 function buildPage(){
@@ -178,7 +207,10 @@ h1 span{\
 }'
 	var style = document.createElement('style')
 	style.type = 'text/css'
-	style.appendChild(document.createTextNode(css))
+	if (style.styleSheet)
+	    style.styleSheet.cssText = css
+	else
+	    style.appendChild(document.createTextNode(css))
 	var head = document.getElementsByTagName('head')[0]
 	var title = document.createElement('title')
 	title.appendChild(document.createTextNode('SuperRefresh'))
@@ -190,7 +222,7 @@ h1 span{\
     frame = document.getElementById('frame')
     urlBar = document.getElementById('urlBar')
     setFrameHeight()
-    window.addEventListener('resize', onWindowResize, false)
+    onEvent(window, 'resize', onWindowResize)
 }
 
 function init(url){
@@ -198,7 +230,7 @@ function init(url){
     blankOutPage()
     buildPage()
     frame = document.getElementById('frame')
-    frame.addEventListener('load', onFrameLoaded, false)
+    onEvent(frame, 'load', onFrameLoaded)
     frame.src = addRandom(mainURL)
 }
 
